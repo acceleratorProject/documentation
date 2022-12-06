8d0591d865427acc4e671949b8c47bc794cd4f0d
Alberto Rivera <albertorivera@MacBook-Pro-de-Alberto.local>
2022-12-06 15:15:21 +0100

    feat: add code scafolding

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <Image src={"/thunder.png"} alt="thunder" width="50" height="50"/>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/acceleratorProject',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/uyHtQ9cUnq',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/acceleratorProject/documentation',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Accelerator Docs',
   },
 }
 
